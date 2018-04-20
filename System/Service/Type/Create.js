fb6cf11e645abf47da3aa3c7c56548e77c583a12
Matt Pike <matt@fakeemailaddress.com>
2018-04-20 12:08:32 +0300

    first commit

---
+++ System/Service/Type/Create.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Matt Pike on 20/04/2018.
+ */
+var Create = (function () {
+    function Create() {
+    }
+    return Create;
+}());
+//# sourceMappingURL=Create.js.map
---
+++ System/Service/Type/Create.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by Matt Pike on 20/04/2018.
+ */
+class Create implements Service{
+
+
+
+}
